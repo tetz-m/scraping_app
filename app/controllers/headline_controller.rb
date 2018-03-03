4e0414bebd3e4329a032833779603706eb1cf6d2
tetz-m <10517313+tetz-m@users.noreply.github.com>
2018-03-03 21:43:53 +0900

    Add update date and time

--- app/controllers/headline_controller.rb
+++ app/controllers/headline_controller.rb
@@ -13,5 +13,11 @@ class HeadlineController < ApplicationController
     #日経 xTECH ランキング：総合
     page = agent.get('http://tech.nikkeibp.co.jp/ranking/')
     @tech_link = page.search('.list_rank h3 a')
+
+    #更新日時の取得
+    weekdays = %w(日 月 火 水 木 金 土)
+    t = Time.now
+    wd = t.strftime("#{weekdays[t.wday]}")
+    @time = t.strftime("%Y年%m月%d日(#{wd}) %H時%M分%S秒")
   end
 end
