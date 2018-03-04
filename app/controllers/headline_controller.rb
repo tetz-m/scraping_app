6298ed541fc1139685c11ccbfc6f2b0cdc400a39
tetz-m <10517313+tetz-m@users.noreply.github.com>
2018-03-05 02:44:10 +0900

    Add local weather news

--- app/controllers/headline_controller.rb
+++ app/controllers/headline_controller.rb
@@ -1,6 +1,19 @@
 class HeadlineController < ApplicationController
   def scraping
+    #更新日時
+    weekdays = %w(日 月 火 水 木 金 土)
+    t = Time.now
+    wd = t.strftime("#{weekdays[t.wday]}")
+    @time = t.strftime("%Y年%m月%d日(#{wd}) %H時%M分%S秒")
+
     agent = Mechanize.new
+
+    #天気予報
+    page = agent.get('https://tenki.jp/forecast/7/')
+    @tenki_link = page.search('#forecast-map-entry-32201')
+    @tenki_date = page.search('#forecast-public-date').text
+
+    @tenki_img = page.search('#forecast-map-entry-32201 img').attribute("src")
 
     #Yahoo! JAPAN IT
     page = agent.get('https://news.yahoo.co.jp/list/?c=computer')
@@ -13,12 +26,6 @@ class HeadlineController < ApplicationController
     #日経 xTECH ランキング：総合
     page = agent.get('http://tech.nikkeibp.co.jp/ranking/')
     @tech_link = page.search('.list_rank h3 a')
-
-    #更新日時の取得
-    weekdays = %w(日 月 火 水 木 金 土)
-    t = Time.now
-    wd = t.strftime("#{weekdays[t.wday]}")
-    @time = t.strftime("%Y年%m月%d日(#{wd}) %H時%M分%S秒")
   end
 
   def update
